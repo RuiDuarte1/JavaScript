aa62ce6f9d6313e195e36e0b5d3aca9a35ceb084
RuiDuarte1 <ruycruzeiro9@gmail.com>
2022-09-25 10:20:32 -0300

    atividades

---
+++ form.js
@@ -0,0 +1,78 @@
+import inquirer from "inquirer";
+import chalk from "chalk";
+
+const formulario = [ 
+
+    {
+    type: 'input',
+    name: 'nome',
+    message: 'Digite o seu nome: '  
+    },
+
+    {
+    type: 'number',
+    name: 'idade',
+    message: 'Digite sua idade'
+    },
+
+    {
+    type: 'checkbox',
+    name: 'sexo',
+    message: 'Marque a opção do seu sexo: ',
+    choices: ["Masculino", "Feminino"]    
+    },
+
+    {
+    type: 'input',
+    name: 'email',
+    message: 'Digite seu e-mail: '  
+    },
+
+    {
+    type: 'password',
+    name: 'senha',
+    message: 'Digite sua senha: '   
+    },
+
+    {
+    type: 'input',
+    name: 'profissao',
+    message: 'Digite sua profissão: '
+    },
+
+    {
+      type: 'checkbox',
+      name: 'civil',
+      message: 'Estado Civil:  ',
+      choices: ["Solteiro", "Casado"]
+      },
+
+    {
+      type: 'input',
+      name: 'telefone',
+      message: "Digite seu telefone: ",
+      validate(value) {
+      const pass = value.match(
+        /^([01]{1})?[-.\s]?\(?(\d{3})\)?[-.\s]?(\d{3})[-.\s]?(\d{4})\s?((?:#|ext\.?\s?|x\.?\s?){1}(?:\d+)?)?$/i
+      );
+      if (pass) {
+        return true;
+      }
+      return 'Insira um número de telefone válido';
+    },
+  },
+
+];
+
+inquirer.prompt(formulario).then(function(answers) {
+  console.log("Nome: ", chalk.green(answers.nome));
+  console.log("Idade: ", chalk.green(answers.idade));
+  console.log("Sexo: ", answers.sexo);
+  console.log("Email: ", chalk.green(answers.email));
+  console.log("Senha: ", chalk.green(answers.senha));
+  console.log("Profissão: ", chalk.green(answers.profissao));
+  console.log("Estado civil: ", answers.civil);
+  console.log('Telefone: ', chalk.green(answers.telefone));
+  
+});
+
